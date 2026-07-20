c639a0287e48ec66ae8ff015b863c79b19ac0ca4
admin <admin@adminui-MacBookAir.local>
2026-07-20 21:13:38 +0900

    add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 
@@ -13,14 +13,22 @@ export const metadata: Metadata = {
     "Reserve tickets for the 2026 Dokteuk Dance Academy Summer Concert.",
 };
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className={`${geistSans.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
